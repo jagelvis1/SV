764233450af8da5e3b046cd245118f88093253eb
jagelvis1 <josegagelvis@gmail.com>
2018-06-03 23:58:14 -0400

    dlventas modelo

--- apps/ventas/models.py
+++ apps/ventas/models.py
@@ -19,7 +19,7 @@ class factura(models.Model):
 #class detalle_factura
 class detalle_factura(models.Model):
     id_detalle_factura = models.AutoField(primary_key=True)    
-    factura = models.ForeignKey(factura, null=True, blank=True, on_delete=models.CASCADE)
+    factura = models.ForeignKey(factura, null=True, blagit add README.mdnk=True, on_delete=models.CASCADE)
     descripcion = models.TextField()
     cantidad = models.IntegerField()
     precio_unit = models.DecimalField(decimal_places=2,max_digits=11)
