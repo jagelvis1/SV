516372003b0a9d868c619b8fddfe368a39ade22c
jagelvis1 <josegagelvis@gmail.com>
2018-06-11 17:38:38 -0400

    mostrar lista de clientes en desarrollo, no funciona el objects

--- apps/persona/urls.py
+++ apps/persona/urls.py
@@ -3,9 +3,9 @@ from __future__ import absolute_import
 
 from django.conf.urls import url, include
 
-from apps.persona.views import index, PersonaView
+from apps.persona.views import index, personaView
 
 urlpatterns = [
     url(r'^$', index, name='index'),
-	url(r'^nuevo$', PersonaView, name='PersonaCreate'),    
+    url(r'^nuevo$', personaView, name='personaView'),
 ]
--- apps/persona/views.py
+++ apps/persona/views.py
@@ -1,20 +1,28 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from __future__ import absolute_import
 
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from apps.persona.PersonaCreate import PersonaForm
+from apps.persona.forms import personaForm
+from apps.persona.models import cliente
 # Create your views here.
 def index(request):
     return render(request,'persona/index.html')
 
-def PersonaView(request):
-	if request.method == 'POST':
-		form = PersonaForm(request.POST)
-		if form.is_valid():
-			form.save()
-		return redirect('persona:index')
-	else:
-		form = PersonaForm()
+#crear cliente
+def personaView(request):
+    if request.method == 'POST':
+        form = personaForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('persona:index') 
+    else:
+        form = personaForm()
+    
+    return render(request,'persona/personaForm.html',{'form':form})  
 
-	return render(request, 'persona/CreatePersona.html', {'form':form})			  
+#Mostrar Cliente
+def personaList(request):
+    persona = cliente.objects.all().order_by('id_cliente')
+    contexto = {'persona':persona}
+    return render(request, 'persona/index.html',contexto)
